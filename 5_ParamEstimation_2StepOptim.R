8fd363d9b6a3d5e1fcb2640a4cb8cbbb134edc98
kkremers <kkremers@nd.edu>
2015-05-05 18:09:44 -0400

    Added litter C and N pools to optimization

--- 5_ParamEstimation_2StepOptim.R
+++ 5_ParamEstimation_2StepOptim.R
@@ -7,7 +7,7 @@
 
 #Get data ready
 head(out) #this is the output from the model run
-data.assim = out[,c(1:3,8,9,11,12,13)] #choose columns that you want
+data.assim = out[,c(1:5,8,9,11,12,13)] #choose columns that you want
 head(data.assim) #preview table
 
 #####remove some data points for Stock data
@@ -22,6 +22,16 @@ BiomassN.assim = data.assim$Biomass_N[match(time.keepSTOCK, data.assim$time)]  #
 BiomassN.assim = data.frame(time=time.keepSTOCK, BiomassN.assim) #create a dataframe of the new data and the corresponding timesteps
 head(BiomassN.assim)
 data.assim$Biomass_N=BiomassN.assim$BiomassN.assim[match(data.assim$time, BiomassN.assim$time)] #change the data in the assimilation table to NAs
+#Litter_C
+LitterC.assim = data.assim$Litter_C[match(time.keepSTOCK, data.assim$time)]  #create vector of data for only those timesteps
+LitterC.assim = data.frame(time=time.keepSTOCK, LitterC.assim) #create a dataframe of the new data and the corresponding timesteps
+head(LitterC.assim) #check table
+data.assim$Litter_C=LitterC.assim$LitterC.assim[match(data.assim$time, LitterC.assim$time)] #change the data in the assimilation table to NAs
+#Litter_N
+LitterN.assim = data.assim$Litter_N[match(time.keepSTOCK, data.assim$time)]  #create vector of data for only those timesteps
+LitterN.assim = data.frame(time=time.keepSTOCK, LitterN.assim) #create a dataframe of the new data and the corresponding timesteps
+head(LitterN.assim)
+data.assim$Litter_N=LitterN.assim$LitterN.assim[match(data.assim$time, LitterN.assim$time)] #change the data in the assimilation table to NAs
 #Available_N
 AvailN.assim = data.assim$Available_N[match(time.keepSTOCK, data.assim$time)]  #create vector of data for only those timesteps
 AvailN.assim = data.frame(time=time.keepSTOCK, AvailN.assim) #create a dataframe of the new data and the corresponding timesteps
@@ -56,6 +66,8 @@ head(data.assim) #preview
 par(mfrow=c(3,2), mar=c(4,4,4,4))
 plot(data.assim$Biomass_C~data.assim$time, pch=16, ylab="Biomass_C", xlab="Time (days)")
 plot(data.assim$Biomass_N~data.assim$time, pch=16, ylab="Biomass_N", xlab="Time (days)")
+plot(data.assim$Litter_C~data.assim$time, pch=16, ylab="Litter_C", xlab="Time (days)")
+plot(data.assim$Litter_N~data.assim$time, pch=16, ylab="Litter_N", xlab="Time (days)")
 plot(data.assim$Available_N~data.assim$time, pch=16, ylab="Available_N", xlab="Time (days)")
 plot(data.assim$GPP~data.assim$time, pch=16, ylab="GPP", xlab="Time (days)")
 plot(data.assim$NEE~data.assim$time, pch=16, ylab="NEE", xlab="Time (days)")
@@ -63,7 +75,7 @@ plot(data.assim$Re~data.assim$time, pch=16, ylab="Re", xlab="Time (days)")
 plot(data.assim$NDVI~data.assim$time, pch=16, ylab="NDVI", xlab="Time (days)")
 
 head(data.assim)
-data.compare1 = data.assim[,c(1:5,7,8)] #pull out columns for data that you want to assimilate
+data.compare1 = data.assim[,c(1:10)] #pull out columns for data that you want to assimilate
 sigma.obs1 = data.frame(matrix(1, length(data.compare1$time), length(data.compare1))) #observation errors for each data type 
 sigma.obs1[,1] = data.assim$time
 colnames(sigma.obs1) = colnames(data.compare1)
@@ -143,7 +155,7 @@ for (i in 2:M) {
     } else { #if there are no NAs or negative stocks
   
   #pull out predicted values to compare to data; only include time points where data is available and columns that match data.compare
-  out.compare1 = out[match(data.compare1$time, out$time),c(1:3,8,9,12,13)] #these columns need to match the ones that were pulled out before
+  out.compare1 = out[match(data.compare1$time, out$time),c(1:5,8,9,11,12,13)] #these columns need to match the ones that were pulled out before
   
   error.time=matrix(0, length(data.compare1$time), D) #create data frame to store error calculations; want all to be "0" originally because if there is no data it will remain 0
   for (d in 1:D) { #for each data type
@@ -217,7 +229,7 @@ save.image(file="Step1_NEE_GPP_Re_BiomassCN_AvailableN.Rdata")
 
 out = data.frame(solvemodel(param.best, state)) #run model
 #pull out predicted values to compare to data; only include time points where data is available and columns that match data.compare
-out.compare1 = out[match(data.compare1$time, out$time),c(1:3,8,9,12,13)] #these columns need to match the ones that were pulled out before
+out.compare1 = out[match(data.compare1$time, out$time),c(1:5,8,9,11,12,13)] #these columns need to match the ones that were pulled out before
 head(out.compare1)
 head(data.compare1)
 
@@ -284,7 +296,7 @@ repeat { #repeat until desired number of parameter sets are accepted
     }#end of repeat loop
     
   #pull out predicted values to compare to data; only include time points where data is available and columns that match data.compare
-  out.compare1 = out[match(data.compare1$time, out$time),c(1:3,8,9,12,13)] #these columns need to match the ones that were pulled out before
+  out.compare1 = out[match(data.compare1$time, out$time),c(1:5,8,9,11,12,13)] #these columns need to match the ones that were pulled out before
   #remove the time column - no longer needed
   data.comp = data.compare1[,-1]
   out.comp = out.compare1[,-1]
