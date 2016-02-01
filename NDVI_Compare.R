9bf3f5e2b15267d8d011f2af281058e1d94da004
kkremers <kkremers@nd.edu>
2016-02-01 16:31:00 -0500

    NDVI regressions

--- NDVI_Compare.R
+++ NDVI_Compare.R
@@ -5,15 +5,24 @@
 data = data.frame(read.csv("NDVI_Compare.csv"))
 head(data)
 
-
 #look at relationships with NDVI NOT AVERAGED and MODIS INTERPOLATED
-Tower_Unispec = lm(data$UniSpec~data$Tower)
+Tower_Unispec = lm(data$NDVI_UniSpec~data$NDVI_7avg)
 summary(Tower_Unispec)
-plot(data$Tower,data$UniSpec, pch=16)
+plot(data$NDVI_7avg,data$NDVI_UniSpec, pch=16)
 abline(Tower_Unispec)
 
-Unispec_MODIS = lm(data$MODIS~data$UniSpec)
+
+#load other data
+data = data.frame(read.csv("NDVI_Compare2.csv"))
+head(data)
+
+Tower_Unispec2 = lm(data$NDVI_UniSpec~data$NDVI_Rad)
+summary(Tower_Unispec2)
+plot(data$NDVI_Rad,data$NDVI_UniSpec, pch=16)
+abline(Tower_Unispec2)
+
+Unispec_MODIS = lm(data$NDVI_MODIS~data$NDVI_UniSpec)
 summary(Unispec_MODIS)
-plot(data$UniSpec,data$MODIS, pch=16)
+plot(data$NDVI_UniSpec,data$NDVI_MODIS, pch=16)
 abline(Unispec_MODIS)
 
